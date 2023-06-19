38b2df689759b170f851396f632c720b9ee0a1af
Yan Gordeev <black-serpent@yandex.ru>
2023-06-19 23:19:11 +0200

    Store integration with Typescript

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,45 +1,59 @@
-import { createStore, Commit, GetterTree } from 'vuex';
+import {
+  createStore,
+  ActionTree,
+  GetterTree,
+  MutationTree,
+} from 'vuex';
 
 import type { IState } from '@/interfaces/State.interface';
 import type { IUser } from '@/interfaces/User.interface';
 
+const state: IState = {
+  users: [],
+};
+
+const mutations: MutationTree<IState> = {
+  setUsers(state, users: IUser[]) {
+    state.users = users;
+  },
+
+  addUser(state, user: IUser) {
+    state.users.push(user);
+  },
+};
+
+const actions: ActionTree<IState, IState> = {
+  setUsers({ commit }) {
+    const storedUsers = localStorage.getItem('users');
+    const users = storedUsers ? JSON.parse(storedUsers) : [];
+    commit('setUsers', users);
+  },
+
+  addUser({ commit, getters }, user: IUser) {
+    const updatedUsers = [...getters.getUsers as IUser[], user];
+    commit('addUser', user);
+    localStorage.setItem('users', JSON.stringify(updatedUsers));
+  },
+}
+
+const getters: GetterTree<IState, IState> = {
+  getUsers: state => state.users,
+
+  getRandomUserInitials: state => {
+    const randomIndex = Math.floor(Math.random() * state.users.length);
+    const randomUser = state.users[randomIndex];
+
+    const initials = `${randomUser.name[0].toUpperCase()}. ${randomUser.lastname[0].toUpperCase()}.`
+
+    return initials
+  }
+}
+
 const store = createStore({
-  state:  {
-    users: [],
-  },
-  mutations: {
-    setUsers(state: IState, users: IUser[]) {
-      state.users = users;
-    },
-    addUser(state: IState, user: IUser) {
-      state.users.push(user);
-    },
-  },
-  actions: {
-    setUsers({ commit } : { commit: Commit }) {
-      const storedUsers = localStorage.getItem('users');
-      const users = storedUsers ? JSON.parse(storedUsers) : [];
-      commit('setUsers', users);
-    },
-    addUser({ commit, getters } : { commit: Commit, getters: GetterTree<IState, IState> }, user: IUser) {
-      const updatedUsers = [...getters.getUsers, user];
-      commit('addUser', user);
-      localStorage.setItem('users', JSON.stringify(updatedUsers));
-    },
-  },
-  getters: {
-    getUsers: (state: IState) => { 
-      return state.users 
-    },
-    getRandomUserInitials: (state: IState) => {
-      const randomIndex = Math.floor(Math.random() * state.users.length);
-      const randomUser = state.users[randomIndex];
-
-      const initials = `${randomUser.name[0].toUpperCase()}. ${randomUser.lastname[0].toUpperCase()}.`
-
-      return initials
-    }
-  },
+  state,
+  mutations,
+  actions,
+  getters,
 })
 
 export default store;
