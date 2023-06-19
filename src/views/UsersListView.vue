9c2842a8492e03388af6084e87d40a5fe2140be2
ncapric <ncapric@griddynamics.com>
2023-06-19 12:48:44 +0200

    Code format

--- src/views/UsersListView.vue
+++ src/views/UsersListView.vue
@@ -8,28 +8,28 @@
 </template>
 
 <script setup lang="ts">
-import { useStore } from 'vuex';
 import { computed, onUnmounted } from 'vue';
+import { useStore } from 'vuex';
 
 import { AgGridVue } from 'ag-grid-vue3';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
-
-const store = useStore();
-const rowData = computed(() => {
-  return store.state.users;
-}
-);
-
-onUnmounted(() => {
-  store.dispatch('setUsers');
-});
 
 const columnDefs = [
   { headerName: 'Name', field: 'name' },
   { headerName: 'Lastname', field: 'lastname' },
   { headerName: 'Age', field: 'age' }
 ];
+
+const store = useStore();
+const rowData = computed(() => {
+  return store.getters.getUsers;
+}
+);
+
+onUnmounted(() => {
+  store.dispatch('setUsers');
+});
 
 </script>
 
