288a4f9d1e8dcefdab4ecb1102076528ea40e807
ncapric <ncapric@griddynamics.com>
2023-06-19 11:09:50 +0200

    Set users on mount

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,18 @@
 </template>
 
 <script setup lang="ts">
-import { RouterView } from "vue-router";
+import { onMounted } from 'vue';
+import { RouterView } from 'vue-router';
+import { useStore } from 'vuex';
+
+const store = useStore();
+
+onMounted(() => {
+  const locallyStoredUsers = JSON.parse(localStorage.getItem('users'));
+
+  store.dispatch('setUsers', locallyStoredUsers);
+});
+
 </script>
 
 <style scoped></style>
